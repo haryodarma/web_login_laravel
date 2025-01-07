ec350dfc73574e7d66ba6c516c12c4e9fde53c7b
haryodarma <haryo.darmap@gmail.com>
2025-01-07 09:28:16 +0800

    add: User Controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    // Show Login Page
+    public function login()
+    {
+    }
+
+    // Login Action
+    public function doLogin()
+    {
+    }
+
+    // LogOut Action
+    public function doLogout()
+    {
+    }
+}
